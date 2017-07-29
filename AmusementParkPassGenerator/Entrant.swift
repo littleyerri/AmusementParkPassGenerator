184f63b23d4ab6a3a1628538b641541eeb147ef9
Gerardo Zayas <jerry@zayas.com>
2017-07-29 12:01:08 -0400

    Initial Commit

---
+++ AmusementParkPassGenerator/Entrant.swift
@@ -0,0 +1,42 @@
+//
+//  Entrant.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Gerardo Zayas on 7/29/17.
+//  Copyright © 2017 Little Yerri. All rights reserved.
+//
+
+import Foundation
+
+protocol Entrant {
+    
+}
+
+protocol EmployeeEntrant: Entrant {
+    
+}
+
+protocol GuestEntrant: Entrant {
+    
+}
+
+protocol EntrantType {
+    
+}
+
+enum EmployeeType: EntrantType {
+    
+}
+
+enum GuestType: EntrantType {
+    
+}
+
+
+
+
+
+
+
+
+
