ba3807b01f5292b94863c6d9fb9924d8b7f48bd3
Gerardo Zayas <jerry@zayas.com>
2017-07-29 15:17:59 -0400

    Code clean up.

--- AmusementParkPassGenerator/Access.swift
+++ AmusementParkPassGenerator/Access.swift
@@ -11,7 +11,7 @@ import Foundation
 typealias percent = Int
 
 protocol Access {
-    // Empty protocol to create an overall Access Type
+    // Empty protocol to create an overall Access Type.
 }
 
 enum AreaAccess: Access {
